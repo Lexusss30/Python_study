6e45551a0c9b31653109c4b7b9dd0393c61418d1
Lexusss30 <a.borodin69@mail.ru>
2024-11-17 18:50:31 +0400

    dzz

---
+++ lesson_threading.py
@@ -0,0 +1,44 @@
+# import threading
+# import time
+
+
+
+# пример без потока
+# def fun_1():
+#     time.sleep(5)
+#     print("end")
+
+# fun_1()
+# for i in range(10):
+#     print(i)
+
+
+
+
+
+# пример с потоком
+# threading.Thread - создание потока
+# thread_1 = threading.Thread(target = fun_1)
+# .start() - запуск потока 
+# thread_1.start()
+# for i in range(10):
+#     print(i)
+
+
+
+
+# создание и запуск второго потока
+# def fun_2():
+#     time.sleep(2)
+#     print("fun_2 end")
+# thread_2 = threading.Thread(target = fun_2)
+
+# thread_2.start()
+
+
+
+
+
+# .join() - строки ниже ждут окончание потока
+# thread_1.join()
+# print("hello")
---
+++ lesson_time.py
@@ -0,0 +1,7 @@
+# подключаем библитеку time
+# import time
+
+# print("hello")
+# .sleep(s) - программа останавливается на s секунд
+# time.sleep(5)
+# print("world")
