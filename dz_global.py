7cad880cc365657481aeca4bc33d3aae547494ca
Lexusss30 <a.borodin69@mail.ru>
2024-12-26 15:01:06 +0300

    dzz

--- dz_global.py
+++ dz_global.py
@@ -632,22 +632,51 @@
 # Задание 1
 
 from tkinter import*
+from tkinter import ttk
+
+
 
 window = Tk()
 window.title("Касса")
 window.geometry("700x500")
 window.config(bg="#c4efef")
 
-def buy():
-    roster_listbox.insert(0 , product_ent.get())
-addition_btn = Button(text= "Добавить товар", fg="#000", command=buy)
-addition_btn.place(x=330, y=345)
-
 
 def delet_product():
     roster_listbox.delete(0, roster_listbox.curselection())
+
+
 deleted_btn = Button(text= "Удалить товар", fg="#000", command=delet_product)
-deleted_btn.place(x=335, y=385)
+deleted_btn.place(x=10, y=550)
+
+addition_btn = Button(text= "Добавить товар", fg="#000")
+addition_btn.place(x=550, y=330)
+
+list_product_lbl = Label(text="Список товаров:", font =(" Arial", 12) )
+list_product_lbl.place(x=550, y=150)
+
+bread = "Хлеб"
+milk = "Молоко"
+chicken_eggs = "Куриные яйца"
+potato = "Картофель"
+carrot = "Морковь"
+
+product_choice = StringVar(value="Хлеб")
+
+first_product_rbtn = ttk.Radiobutton(text= "Хлеб", variable=product_choice, value="Хлеб")
+first_product_rbtn.place(x=550, y=180)
+
+second_product_rbtn = ttk.Radiobutton(text= "Молоко", variable=product_choice, value="Молоко")
+second_product_rbtn.place(x=550, y=210)
+
+third_product_rbtn = ttk.Radiobutton(text= "Яйца куриные", variable=product_choice, value="Яйца куриные")
+third_product_rbtn.place(x=550, y=240)
+
+fourth_product_rbtn = ttk.Radiobutton(text= "Картофель", variable=product_choice, value="Картофель")
+fourth_product_rbtn.place(x=550, y=270)
+
+fifth_product_rbtn = ttk.Radiobutton(text= "Морковь", variable=product_choice, value="Морковь")
+fifth_product_rbtn.place(x=550, y=300)
 
 purchases_lbl = Label(text="Список покупок:", font =(" Arial", 12))
 purchases_lbl.place(x=200, y=150)
@@ -655,16 +684,6 @@ purchases_lbl.place(x=200, y=150)
 array_product = []
 roster_listbox = Listbox(listvariable= Variable(value=array_product))
 roster_listbox.place(x=200, y=180)
-
-product_ent= Entry(bg="#f7f7f7", fg="#0d0c0d", font=("Times New Roman", 9))
-product_ent.place(x=200, y=345, height=27)
-
-def cost_product():
-#итоговая сумма покупок
-    1.
-
-result_lbl = Label(text="\nИтоговая сумма:\nрублей", font =(" Arial", 10))
-result_lbl.place(x=50, y=345)
 
 window.mainloop()
 
